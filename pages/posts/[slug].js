6c79ce3073e250a06b557999c3c29e9005d6103c
StephenAraka <stevenaraka@gmail.com>
2022-06-03 15:18:49 +0300

    render single blog

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,6 @@
 import styles from '../../styles/Slug.module.css';
 import { GraphQLClient, gql } from 'graphql-request';
+import moment from "moment";
 
 const graphcms = new GraphQLClient('https://api-eu-central-1.graphcms.com/v2/cl3odbzz75jbv01xicmr4333w/master');
 
@@ -36,7 +37,7 @@ const SLUGLIST = gql`
 `;
 
 export async function getStaticPaths() {
-  const { posts } = graphcms.request(SLUGLIST);
+  const { posts } = await graphcms.request(SLUGLIST);
 
   return {
     paths: posts.map((post) => ({ params: { slug: post.slug } })),
@@ -44,12 +45,39 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps() {
-  const { posts } = await graphcms.request(QUERY);
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+
+  const data = await graphcms.request(QUERY, { slug });
+
+  const post = data.post;
+
   return {
     props: {
-      posts,
+      post,
     },
     revalidate: 10
   }
+}
+
+export default function BlogPost({ post }) {
+  return (
+    <main className={styles.blog}>
+      <img src={post.coverPhoto.url} className={styles.cover} alt="cover photo" />
+      <div className={styles.title}>
+        <div className={styles.authdetails}>
+          <img src={post.author.avatar.url} alt={post.author.name} />
+          <div className={styles.authtext}>
+            <h6>By {post.author.name}</h6>
+            <h6 className={styles.date}> {moment(post.datePublished).format("MMMM d, YYYY")}</h6>
+          </div>
+        </div>
+        <h2>{post.title}</h2>
+      </div>
+
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: post.content.html }} ></div>
+    </main>
+  )
 }
