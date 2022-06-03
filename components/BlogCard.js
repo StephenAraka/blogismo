ef515b954b91253aa5be9a9cb6935d015ef308e0
StephenAraka <stevenaraka@gmail.com>
2022-06-03 15:33:38 +0300

    fix blog card image

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,24 +1,26 @@
 import Link from 'next/link';
 import styles from '../styles/BlogCard.module.css';
+import Image from "next/image";
+import moment from "moment";
 
 export default function BlogCard({ title, author, coverPhoto, datePublished, slug }) {
     return (
         <div className={styles.card}>
             <Link href={`/posts/${slug}`}>
                 <div className={styles.imgContainer}>
-                    <img src={coverPhoto.url} alt="cover photo" />
+                    <Image layout='fill' src={coverPhoto.url} alt="cover photo" />
                 </div>
             </Link>
             <div className={styles.text}>
                 <h2>{title}</h2>
-            <div className={styles.details}>
-                <div className={styles.author}>
-                    <img src={author.avatar.url} alt="author" />
-                    <h3>{author.name}</h3>
-                </div>
-                <div className={styles.date}>
-                    <h3>{datePublished}</h3>
-                </div>
+                <div className={styles.details}>
+                    <div className={styles.author}>
+                        <img src={author.avatar.url} alt="author" />
+                        <h3>{author.name}</h3>
+                    </div>
+                    <div className={styles.date}>
+                        <h3>{moment(datePublished).format("MMMM d, YYYY")}</h3>
+                    </div>
                 </div>
             </div>
         </div>
